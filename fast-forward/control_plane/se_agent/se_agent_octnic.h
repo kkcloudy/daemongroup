059442fc6cc72c3876e266cb270abb2df8c4ce0c
sushaohua <sush@autelan.com>
2013-05-10 15:25:58 +0800

    removed fastfwd from dg.

--- fast-forward/control_plane/se_agent/se_agent_octnic.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SE_AGENT_OCTNIC_H_
-#define _SE_AGENT_OCTNIC_H_
-
-/* pcie interface function */
-int32_t se_agent_octeon_init();
-int32_t se_agent_send_fccp_from_pci(uint8_t* fccp_send, int32_t slen, uint8_t* fccp_rcv, int32_t rlen);
-
-#endif
-
--- fast-forward/data_plane/fast-fwd/builddate.c
+++
@@ -1,5 +0,0 @@
-#include "executive-config.h"
-#include "builddate.h"
-
-const char version_string[] =
-	" "FASTFWD_BUILD_NUMBER_STR" (""Build time: " __DATE__ " - " __TIME__ ")";
