c2287b1b8e69f1573bf8c80bc23b2aecda32776c
caojia <caojia@new17.(none)>
2014-02-28 17:59:44 +0800

    Cao Jia sync sp7new to sp8new : Dong Tao bugfix for trap port updown and board remove.

--- dcli/src/lib/dcli_snmp.c
+++ dcli/src/lib/dcli_snmp.c
@@ -5069,7 +5069,7 @@ DEFUN(set_trap_type_switch_func,
 					"21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|"\
 					"41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|"\
 					"61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|"\
-					"81|82|83|84|85|86|87|88|89|90|91|92|93|94) (on|off)",
+					"81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99) (on|off)",
 	SETT_STR
 	"snmp trap \n"
 	"snmp trap type \n"
@@ -5167,6 +5167,11 @@ DEFUN(set_trap_type_switch_func,
 	/*92*/"AP Configuration Error\n"
 	/*93*/"User Traffic Overload \n"
 	/*94*/"Unauthorized Station Mac\n"
+	/*95*/"ac Board  Extract\n"
+	/*96*/"ac Boadr  Insert\n"
+	/*97*/"ac port Down\n"
+	/*98*/"ac port Up\n"
+	/*99*/"ap Traffic Overload\n"
 	"trap switch on \n"
 	"trap switch off \n"
 )
